d9d3226fb9355629b9e7a3dfc3079681d7bc23d5
oddi80 <oddi80@proton.me>
2026-09-07 19:33:46 +0200

    Add files via upload

---
+++ F_to_C.py
@@ -0,0 +1,14 @@
+"""
+Created on Sun Sep  6 15:33:22 2026
+
+@author: oddi80
+"""
+#%%
+
+F = float(input("Skriv inn grader Fahrenheit: "))
+C = (F - 32)*(5/9)
+
+
+
+print(f"    {F:.2f} grader Fahrenheit \n==> {C:.2f} grader Celsius")
+
